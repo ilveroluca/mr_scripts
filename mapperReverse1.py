5c937e817bef6589845ae5597d349058b9a5593d
alexey <alexey@master.hadoop.vm>
2013-09-23 21:37:39 +0200

    mapper for forward reads

--- mapperReverse1.py
+++ mapperReverse1.py
@@ -9,7 +9,8 @@ def main():
 	lines = read_input(sys.stdin)
     	for line in lines:
 		if (line[0] == "@" and len(line) < 100):
-                        print "%s" %line.split('/')[0]+'.2'
+       #                 print "%s" %line.split('/')[0]+'.2'
+                        print "%s" %''.join(line.split('.'))+'.2'
                 else:
                         print  line
 
