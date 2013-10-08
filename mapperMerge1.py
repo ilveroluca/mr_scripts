f7ae859d53d5f9f1494b741c9590f5951c2d506b
Luca Pireddu <pireddu@crs4.it>
2013-10-08 14:20:38 +0200

    Clean-up whitespace

--- mapperMerge1.py
+++ mapperMerge1.py
@@ -4,37 +4,34 @@ import sys
 
 
 def read_input(files):
-	for line in files:
-	        yield line.strip()
-
-
+    for line in files:
+        yield line.strip()
 
 # input comes from STDIN (standard input)
 def main():
- 	lines_ = read_input(sys.stdin)
-	lines=[]
-	nLines=0
-	firstLocated=False
-   	for line in lines_:
-           # remove leading and trailing whitespace
-        	if not firstLocated:
+    lines_ = read_input(sys.stdin)
+    lines=[]
+    nLines=0
+    firstLocated=False
+    for line in lines_:
+        # remove leading and trailing whitespace
+        if not firstLocated:
         #       print line
-                # since each mapper will get some random chunk of the file, 
-                # the chunk may not start with the FASTQ header.
-                # so, we just skip that read. 
-                # In general we will loose the number of reads about the number of mappers, i.e. less than 100
-                #if line[0]=='@': firstLocated=True; lines.append(''.join(line.split())); nLines=1
-        	        if (line[0]=='@'and len(line) < 100): firstLocated=True; lines.append(''.join(line.split())); nLines=0
-        	else:
-        	        if nLines < 3 :
-        	                nLines+=1
-        	                lines.append(line)
-#                       print line, nLines
-        	        else:
-        #               print '%s\t%s' % ('FN:head_1;RN:'+lines[0]+';', ' '.join(lines[1:])); 
-        	                print '%s\t%s\t%s' % (lines[0], lines[1], lines[3]);
-        	                lines=[];lines.append(''.join(line.split())); nLines=0;
+        # since each mapper will get some random chunk of the file,
+        # the chunk may not start with the FASTQ header.
+        # so, we just skip that read.
+        # In general we will loose the number of reads about the number of mappers, i.e. less than 100
+        #if line[0]=='@': firstLocated=True; lines.append(''.join(line.split())); nLines=1
+        if (line[0]=='@'and len(line) < 100): firstLocated=True; lines.append(''.join(line.split())); nLines=0
+        else:
+            if nLines < 3 :
+                nLines+=1
+                lines.append(line)
+                #   print line, nLines
+            else:
+                #   print '%s\t%s' % ('FN:head_1;RN:'+lines[0]+';', ' '.join(lines[1:]));
+                print '%s\t%s\t%s' % (lines[0], lines[1], lines[3]);
+                lines=[];lines.append(''.join(line.split())); nLines=0;
 
 if __name__ == "__main__":
-	main()
-
+    main()
--- reducerMerge1.py
+++ reducerMerge1.py
@@ -5,19 +5,18 @@ import sys
 prevId = None
 prevRead = None
 for line in sys.stdin:
-        # the mapper uses tab as the separator between key and value.
-        readID,  read = line.strip().split('\t',1)
-#       print '%s'% readID # %(readID, '\t'.join(read.split()) ) #, read
-        if prevId:
-                if prevId.split('.')[0] == readID.split('.')[0]:
-        #       print '%s\t%s' %( prevId, readID )
-                        print '%s\t%s\t%s' %( prevId, '\t'.join(prevRead.split()), '\t'.join(read.split()) )
-                        prevId = None
-                else:
-                        prevId = readID
-                        prevRead = read
+    # the mapper uses tab as the separator between key and value.
+    readID,  read = line.strip().split('\t',1)
+    #   print '%s'% readID # %(readID, '\t'.join(read.split()) ) #, read
+    if prevId:
+        if prevId.split('.')[0] == readID.split('.')[0]:
+            # print '%s\t%s' %( prevId, readID )
+            print '%s\t%s\t%s' %( prevId, '\t'.join(prevRead.split()), '\t'.join(read.split()) )
+            prevId = None
         else:
-                #if prevId: print '%s\t%s' % ( prevId, '\t'.join(prevRead.split()) ) 
-                prevId = readID
-                prevRead = read
-
+            prevId = readID
+            prevRead = read
+    else:
+        #if prevId: print '%s\t%s' % ( prevId, '\t'.join(prevRead.split()) )
+        prevId = readID
+        prevRead = read
