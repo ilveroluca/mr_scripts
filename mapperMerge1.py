1b8b33b3eda0be706b21b79a00addb06324b2b73
Luca Pireddu <pireddu@crs4.it>
2013-10-10 11:33:40 +0200

    Fix whitespace and simplify merge logic

--- mapperMerge1.py
+++ mapperMerge1.py
@@ -5,7 +5,8 @@ import sys
 
 def read_input(files):
     for line in files:
-        yield line.strip()
+        # remove tabs,trailing whitespace and newline
+        yield line.rstrip().replace('\t', ' ')
 
 # input comes from STDIN (standard input)
 def main():
@@ -13,25 +14,31 @@ def main():
     lines=[]
     nLines=0
     firstLocated=False
+
+    # since each mapper will get some random chunk of the file,
+    # the chunk may not start with the FASTQ header.
+    # so, we just skip that read.
+    # In general we will lose the number of reads about the number of mappers, i.e. less than 100
     for line in lines_:
-        # remove leading and trailing whitespace
         if not firstLocated:
-        #       print line
-        # since each mapper will get some random chunk of the file,
-        # the chunk may not start with the FASTQ header.
-        # so, we just skip that read.
-        # In general we will loose the number of reads about the number of mappers, i.e. less than 100
-        #if line[0]=='@': firstLocated=True; lines.append(''.join(line.split())); nLines=1
-        if (line[0]=='@'and len(line) < 100): firstLocated=True; lines.append(''.join(line.split())); nLines=0
+            # since each mapper will get some random chunk of the file,
+            # the chunk may not start with the FASTQ header.
+            # so, we just skip that read.
+            # In general we will lose the number of reads about the number of mappers, i.e. less than 100
+            if line[0]=='@' and len(line) < 100:
+                firstLocated=True
+                lines.append(line)
+                nLines=0
         else:
-            if nLines < 3 :
+            if nLines < 3:
                 nLines+=1
                 lines.append(line)
                 #   print line, nLines
             else:
                 #   print '%s\t%s' % ('FN:head_1;RN:'+lines[0]+';', ' '.join(lines[1:]));
-                print '%s\t%s\t%s' % (lines[0], lines[1], lines[3]);
-                lines=[];lines.append(''.join(line.split())); nLines=0;
+                print '\t'.join( (lines[0], lines[1], lines[3]) )
+                lines = [ line ]
+                nLines = 0
 
 if __name__ == "__main__":
     main()
--- reducerMerge1.py
+++ reducerMerge1.py
@@ -4,19 +4,19 @@ import sys
 
 prevId = None
 prevRead = None
+
 for line in sys.stdin:
     # the mapper uses tab as the separator between key and value.
-    readID,  read = line.strip().split('\t',1)
-    #   print '%s'% readID # %(readID, '\t'.join(read.split()) ) #, read
+    readID, read = line.strip().split('\t', 1)
+    readID = readID[0:-2] # drop the trailing /1 or /2
     if prevId:
-        if prevId.split('.')[0] == readID.split('.')[0]:
-            # print '%s\t%s' %( prevId, readID )
-            print '%s\t%s\t%s' %( prevId, '\t'.join(prevRead.split()), '\t'.join(read.split()) )
+        if prevId == readID:
+            print '\t'.join( (prevId, prevRead, read) )
             prevId = None
         else:
+            # dropping an unpaired read
             prevId = readID
             prevRead = read
     else:
-        #if prevId: print '%s\t%s' % ( prevId, '\t'.join(prevRead.split()) )
         prevId = readID
         prevRead = read
